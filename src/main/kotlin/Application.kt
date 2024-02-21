f2b19b99c56a8aaa91e7874c66557e89387adf17
izzypod5 <izzypod5@googlemail.com>
2024-02-21 22:59:40 +0000

    Additional dependencies added related to embedded server and kotlin htmx related starter

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,9 +1,20 @@
 package org.demo
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
+
+    embeddedServer(Netty, port=8080) {
+        module()
+    }.start(wait = true)
+
+/*    val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
@@ -12,5 +23,13 @@ fun main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
+    }*/
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
     }
 }
